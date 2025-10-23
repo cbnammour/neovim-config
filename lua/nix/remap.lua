51f0107ff76ba6a1a30a952327ebb5d1b0c915e6
Charbel Nammour <charbel.nammour1998@gmail.com>
2025-10-24 02:34:34 +0300

    remap jj to exit insert mode

--- lua/nix/remap.lua
+++ lua/nix/remap.lua
@@ -4,3 +4,10 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>fmt", function()
     vim.lsp.buf.format({ timeout_ms = 2000 })
 end, { desc = "Format code" })
+
+
+-- remap jj to escape insert modern
+vim.keymap.set("i", "jj", "<Esc>")
+-- set a tiemout for escape sequences and enable timeout
+vim.o.timeout = true
+vim.o.timeoutlen = 300
