4d446f4c16b084abd3c096596ec5bc494c7f5b2a
cbnammour <charbel.nammour1998@gmail.com>
2025-10-24 14:17:17 +0000

    autoinstall packer on startup if not available

--- init.lua
+++ init.lua
@@ -1,3 +1,13 @@
+-- Packer bootstrap (safe to keep forever)
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git','clone','--depth','1','https://github.com/wbthomason/packer.nvim',install_path})
+  vim.cmd('packadd packer.nvim')
+end
+
+
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 require("nix")
