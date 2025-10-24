2b7cdb66a1f1f3534a3c2099455d3888df1e5e1e
Charbel Nammour <charbel.nammour1998@gmail.com>
2025-10-24 03:12:44 +0300

    Remove noob keys

--- lua/nix/remap.lua
+++ lua/nix/remap.lua
@@ -11,3 +11,17 @@ vim.keymap.set("i", "jj", "<Esc>")
 -- set a tiemout for escape sequences and enable timeout
 vim.o.timeout = true
 vim.o.timeoutlen = 500
+
+-- remove arrow keymaps
+vim.keymap.set("n", "<Up>", "<cmd>echo 'Use k to move up'<cr>")
+vim.keymap.set("n", "<Down>", "<cmd>echo 'Use j to move down'<cr>")
+vim.keymap.set("n", "<Left>", "<cmd>echo 'Use h to move left'<cr>")
+vim.keymap.set("n", "<Right>", "<cmd>echo 'Use l to move right'<cr>")
+vim.keymap.set("i", "<Up>", "<cmd>echo 'Use k to move up'<cr>")
+vim.keymap.set("i", "<Down>", "<cmd>echo 'Use j to move down'<cr>")
+vim.keymap.set("i", "<Left>", "<cmd>echo 'Use h to move left'<cr>")
+vim.keymap.set("i", "<Right>", "<cmd>echo 'Use l to move right'<cr>")
+vim.keymap.set("v", "<Up>", "<cmd>echo 'Use k to move up'<cr>")
+vim.keymap.set("v", "<Down>", "<cmd>echo 'Use j to move down'<cr>")
+vim.keymap.set("v", "<Left>", "<cmd>echo 'Use h to move left'<cr>")
+vim.keymap.set("v", "<Right>", "<cmd>echo 'Use l to move right'<cr>")
